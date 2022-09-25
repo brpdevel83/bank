a8983dbec4eea66d57e44eb759cb247c8f52631a
Babak Partovi <brpdevelop83@gmail.com>
2022-09-25 15:47:20 +0300

    service and domain created

--- app/app.go
+++ app/app.go
@@ -12,8 +12,8 @@ func Start() {
 	//mux := http.NewServeMux()
 	mux := mux.NewRouter()
 
-	ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryStub())}
-	mux.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	ch := CustomerHandlers{service.NewCustomerService(domain.CustomerRepositoryStub())}
+	mux.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 
 	//mux.HandleFunc("/greet", greet).Methods(http.MethodGet)
 	//mux.HandleFunc("/customers/{id:[0-9]+}", getCustomer).Methods(http.MethodGet)
