acde1e24ea49ac2f32d39b6ca65eaa8ca0101639
Babak Partovi <brpdevelop83@gmail.com>
2022-09-25 14:54:37 +0300

    service and domain created

--- service/customerService.go
+++ service/customerService.go
@@ -1,7 +1,19 @@
 package service
 
-import domain "github.com/brpdevel83/bank/domian"
+import domain "github.com/brpdevel83/bank/domain"
 
 type CustomerService interface {
-	GetAllCustomers() ([]domain.Customer, nil)
+	GetAllCustomers() ([]domain.Customer, error)
+}
+
+type DefaualtCustomerService struct {
+	repo domain.CustomerRepository
+}
+
+func GetAllCustomers(s DefaualtCustomerService) ([]domain.Customer, error) {
+	return s.repo.FindAll()
+}
+
+func NewCustomerService(repository domain.CustomerRepository) DefaualtCustomerService {
+	return DefaualtCustomerService{repository}
 }
